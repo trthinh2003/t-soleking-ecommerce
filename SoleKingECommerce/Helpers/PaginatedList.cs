083caefcec9e1873b16ba50bf963f2afb6de2646
thinhschool1357 <104267661+trthinh2003@users.noreply.github.com>
2025-08-01 17:43:41 +0700

    TSoleKing V1 Done

--- SoleKingECommerce/Helpers/PaginatedList.cs
+++ SoleKingECommerce/Helpers/PaginatedList.cs
@@ -6,8 +6,8 @@ namespace SoleKingECommerce.Helpers
     {
         public int PageIndex { get; private set; }
         public int TotalPages { get; private set; }
-        public int TotalCount { get; private set; }  // Thêm thuộc tính này
-        public int PageSize { get; private set; }    // Thêm thuộc tính này
+        public int TotalCount { get; private set; }
+        public int PageSize { get; private set; }  
 
         public PaginatedList(List<T> items, int count, int pageIndex, int pageSize)
         {
